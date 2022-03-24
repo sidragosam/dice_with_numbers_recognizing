34da1b64ef54995a8674aeaaf4b156c661de5156
Tibor Ignéczi <47614820+sidragosam@users.noreply.github.com>
2022-03-24 13:24:50 +0100

    last commit for today

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ if img is None:
 grayImage = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 (thresh2, blackAndWhiteImage2) = cv.threshold(grayImage, 127, 255, cv.THRESH_BINARY)
 edged2 = cv.Canny(grayImage, 50, 200, 255)
-cv.imshow("Edged2", edged2)
+# cv.imshow("Edged2", edged2)
 new_image = np.zeros(grayImage.shape, grayImage.dtype)
 
 alpha = 1.0  # [1.0-3.0]
@@ -53,7 +53,7 @@ for y in range(grayImage.shape[0]):
 edged = cv.Canny(new_image, 50, 200, 255)
 
 # cv.imshow("Ablak", grayImage)
-cv.imshow("Edged", edged)
+# cv.imshow("Edged", edged)
 
 # thresh = cv.threshold(new_image, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)[1]
 kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (1, 5))
@@ -65,14 +65,14 @@ def inverte(imagem):
     return imagem
 
 
-cv.imshow("Threshold", thresh)
+# cv.imshow("Threshold", thresh)
 
 newthresh = functions.in_range_img(img)
 
-cv.imshow("Uj threshold", newthresh)
+# cv.imshow("Uj threshold", newthresh)
 
 # Read image
-im_in = cv.imread("dice6.jpg", cv.IMREAD_GRAYSCALE)
+im_in = cv.imread("dice.jpg", cv.IMREAD_GRAYSCALE)
 
 # Threshold.
 # Set values equal to or above 220 to 0.
@@ -98,24 +98,24 @@ im_floodfill_inv = cv.bitwise_not(im_th)
 im_out = im_th  # | im_floodfill_inv
 
 # Display images.
-cv.imshow("Thresholded Image", im_th)
-cv.imshow("Floodfilled Image", im_floodfill)
-cv.imshow("Inverted Floodfilled Image", im_floodfill_inv)
-cv.imshow("Foreground", im_out)
+# cv.imshow("Thresholded Image", im_th)
+# cv.imshow("Floodfilled Image", im_floodfill)
+# cv.imshow("Inverted Floodfilled Image", im_floodfill_inv)
+# cv.imshow("Foreground", im_out)
 # cv.waitKey(0)
 
-finalimg = cv.imread("dice2.jpg")
+finalimg = cv.imread("dice.jpg")
 finalthresh = functions.in_range_img(finalimg)
 
 cv.imshow("FinalIMG", finalthresh)
 
-
 options = "outputbase digits"
 custom_config = r'--oem 3 --psm 6 outputbase digits -c tessedit_char_whitelist=123456'
-
 im = Image.fromarray((finalthresh * 255).astype(np.uint8))  # átalakítás 0-1-ről 0-255-re
 print("Számok: " + pytesseract.image_to_string(im, config=custom_config))
-cv.imshow("Output", im_out)
+
+
+# cv.imshow("Output", im_out)
 
 # rgb = cv.cvtColor(invertalt, cv.COLOR_BGR2RGB)
 # text = pytesseract.image_to_string(im_floodfill_inv, config=options)# im_out, config=options)
